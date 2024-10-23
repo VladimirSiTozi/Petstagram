79e0786aff603a840cbf4c30c021092a14afa60f
VladimirSiTozi <132054352+VladimirSiTozi@users.noreply.github.com>
2024-10-23 14:06:18 +0300

    add: pagination fucntionality to the home page + html and css code

--- petstagram/common/views.py
+++ petstagram/common/views.py
@@ -15,7 +15,7 @@ class HomePageView(ListView):
     model = Photo
     template_name = 'common/home-page.html'
     context_object_name = 'all_photos'
-    paginate_by = 3
+    paginate_by = 2
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
